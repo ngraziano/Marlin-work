d09ee8224f3e0611e5528961aec5f83e6955220d
Nicolas Graziano <nicolas.graziano@gmail.com>
2017-04-05 15:38:24 +0200

    Auto start dagoma0.g.

--- Marlin/cardreader.cpp
+++ Marlin/cardreader.cpp
@@ -562,7 +562,11 @@ void CardReader::checkautostart(bool force) {
   #endif
 
   char autoname[10];
-  sprintf_P(autoname, PSTR("auto%i.g"), autostart_index);
+  #if ENABLED(DISABLE_DAGAUTO_START)
+    sprintf_P(autoname, PSTR("auto%i.g"), autostart_index);
+  #else
+    sprintf_P(autoname, PSTR("dagoma%i.g"), autostart_index);
+  #endif
   for (int8_t i = 0; i < (int8_t)strlen(autoname); i++) autoname[i] = tolower(autoname[i]);
 
   dir_t p;
@@ -572,10 +576,14 @@ void CardReader::checkautostart(bool force) {
   bool found = false;
   while (root.readDir(p, NULL) > 0) {
     for (int8_t i = 0; i < (int8_t)strlen((char*)p.name); i++) p.name[i] = tolower(p.name[i]);
-    if (p.name[9] != '~' && strncmp((char*)p.name, autoname, 5) == 0) {
-      openAndPrintFile(autoname);
-      found = true;
-    }
+      #if ENABLED(DISABLE_DAGAUTO_START)
+        if (p.name[9] != '~' && strncmp((char*)p.name, autoname, 5) == 0) {
+      #else
+        if (p.name[9] != '~' && strncmp((char*)p.name, autoname, 7) == 0) {
+      #endif
+          openAndPrintFile(autoname);
+          found = true;
+        }
   }
   if (!found)
     autostart_index = -1;
