be911671b9e121b6d3e97092f86e3a838f115ff7
Nicolas Graziano <nicolas.graziano@gmail.com>
2017-04-05 18:05:46 +0200

    Add discoeasy Pinout.

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -172,6 +172,8 @@
   #include "pins_MKS_13.h"
 #elif MB(SAINSMART_2IN1)
   #include "pins_SAINSMART_2IN1.h"
+#elif MB(DISCOEASY)
+  #include "pins_DISCOEASY.h"
 #else
   #error "Unknown MOTHERBOARD value set in Configuration.h"
 #endif
---
+++ Marlin/pins_DISCOEASY.h
@@ -0,0 +1,48 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * DiscoEasy (MKS_BASE with special pinout)
+ */
+
+#define BOARD_NAME "Disco Easy"
+
+#include "pins_MKS_BASE.h"
+
+// redfine for DISCOEASY
+#undef HEATER_1_PIN
+#define HEATER_1_PIN -1
+#undef PRINTER_HEAD_EASY_CONSTANT_FAN_PIN
+#define PRINTER_HEAD_EASY_CONSTANT_FAN_PIN 7
+#undef X_MAX_PIN
+#define X_MAX_PIN -1
+#undef X_MIN_PIN
+#define X_MIN_PIN 2
+#undef Y_MAX_PIN
+#define Y_MAX_PIN 3
+#undef Y_MIN_MIN
+#define Y_MIN_MIN -1
+#undef Z_MAX_PIN
+#define Z_MAX_PIN -1
+#undef Z_MIN_PIN
+#define Z_MIN_PIN 15
+
